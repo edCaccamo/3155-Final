65c04e2377e0b3cf6f9f7445600bf2c112ee30ef
edCaccamo <ecaccamo144@gmail.com>
2024-12-08 18:53:25 -0500

    second commit, Added schemas todo, orders todo finished

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, customers, payment_info, promotions, reviews
 
 from ..dependencies.database import engine
 
@@ -9,3 +9,7 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    customers.Base.metadata.create_all(engine)
+    payment_info.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -1,28 +1,41 @@
 from datetime import datetime
-from typing import Optional
-from pydantic import BaseModel
+from typing import Optional, List, Annotated
+from pydantic import BaseModel, condecimal
 from .order_details import OrderDetail
+from .promotions import Promotion
+from .customers import Customer
+from .payment_info import PaymentInfo
 
-
+#for formatting price
+ConDecimal4_2 = Annotated[float, condecimal(max_digits=4, decimal_places=2)]
 
 class OrderBase(BaseModel):
-    customer_name: str
-    description: Optional[str] = None
+    status: Optional[str] = None
+    total_price: Optional[ConDecimal4_2] = None
+    customer_id: Optional[int] = None
+    promotion_id: Optional[int] = None
 
 
 class OrderCreate(OrderBase):
-    pass
+    customer_id: int
+    status: str
+    total_price: ConDecimal4_2 = 0.00
 
 
 class OrderUpdate(BaseModel):
-    customer_name: Optional[str] = None
-    description: Optional[str] = None
+    status: Optional[str] = None
+    total_price: Optional[ConDecimal4_2] = None
+    customer_id: Optional[int] = None
+    promotion_id: Optional[int] = None
 
 
 class Order(OrderBase):
     id: int
     order_date: Optional[datetime] = None
     order_details: list[OrderDetail] = None
+    customer: Optional[Customer] = None
+    promotion: Optional[Promotion] = None
+    payment_info: Optional[PaymentInfo] = None
 
     class ConfigDict:
         from_attributes = True
