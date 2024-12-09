1427f88b31da91e1da878c32ac103ae81a806ee8
edCaccamo <ecaccamo144@gmail.com>
2024-12-08 23:29:18 -0500

    Some edits

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, reviews, promotions, payment_info
+from . import orders, order_details, reviews, promotions, payment_info, recipes
 
 
 def load_routes(app):
@@ -7,3 +7,4 @@ def load_routes(app):
     app.include_router(reviews.router)
     app.include_router(promotions.router)
     app.include_router(payment_info.router)
+    app.include_router(recipes.router)
